c33aeb93d38110d25032d392c6650ddfd58202bb
Leo Hong <hongliyu2002@hotmail.com>
2023-03-05 21:27:34 +0800

    Update

--- EMachine.Sales.Domain/SnackRepositoryGrain.cs
+++ EMachine.Sales.Domain/SnackRepositoryGrain.cs
@@ -52,7 +52,7 @@ public class SnackRepositoryGrain : Grain, ISnackRepositoryGrain
                      .Ensure(_snacks.State.Set.Contains(cmd.Id) == false, $"Snack {cmd.Id} already exists.")
                      .TapTry(() => grain = GrainFactory.GetGrain<ISnackGrain>(cmd.Id))
                      .BindTryAsync(() => grain!.InitializeAsync(new SnackInitializeCommand(cmd.Name, cmd.TraceId, cmd.OperatedBy)))
-                     .TapAsync(() => _snacks.State.Set.Add(cmd.Id))
+                     .TapTryAsync(() => _snacks.State.Set.Add(cmd.Id) ? _snacks.WriteStateAsync() : Task.CompletedTask)
                      .BindAsync(() => grain!.GetAsync());
     }
 
@@ -64,7 +64,7 @@ public class SnackRepositoryGrain : Grain, ISnackRepositoryGrain
                      .Ensure(_snacks.State.Set.Contains(cmd.Id), $"Snack {cmd.Id} does not exist.")
                      .TapTry(() => grain = GrainFactory.GetGrain<ISnackGrain>(cmd.Id))
                      .BindTryAsync(() => grain!.RemoveAsync(new SnackRemoveCommand(cmd.TraceId, cmd.OperatedBy)))
-                     .TapAsync(() => _snacks.State.Set.Remove(cmd.Id));
+                     .TapTryAsync(() => _snacks.State.Set.Remove(cmd.Id) ? _snacks.WriteStateAsync() : Task.CompletedTask);
     }
 
     /// <inheritdoc />
@@ -75,5 +75,24 @@ public class SnackRepositoryGrain : Grain, ISnackRepositoryGrain
                      .Ensure(_snacks.State.Set.Contains(cmd.Id), $"Snack {cmd.Id} does not exist.")
                      .TapTry(() => grain = GrainFactory.GetGrain<ISnackGrain>(cmd.Id))
                      .BindTryAsync(() => grain!.ChangeNameAsync(new SnackNameChangeCommand(cmd.Name, cmd.TraceId, cmd.OperatedBy)));
+    }
+
+    /// <inheritdoc />
+    public override async Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        if (_snacks.State.Set.Count == 0)
+        {
+            await InitializeDataAsync();
+        }
+        await base.OnActivateAsync(cancellationToken);
+    }
+
+    private async Task<Result<IEnumerable<Snack>>> InitializeDataAsync()
+    {
+        var results = await Task.WhenAll(CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("a74ad750-0d8b-4ede-ad29-e5e0d5c2dd9e"), "Cafe", Guid.NewGuid(), "System")),
+                                         CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("7db1925a-4458-479f-9c6b-1f708c35b29e"), "Chocolate", Guid.NewGuid(), "System")),
+                                         CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("d5753343-78d4-4c4a-a674-63b75f096059"), "Soda", Guid.NewGuid(), "System")),
+                                         CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("83d423b6-96a3-48fd-b9e7-97a00bb98e2f"), "Gum", Guid.NewGuid(), "System")));
+        return Result.Combine(results);
     }
 }
--- Tests/EMachine.Sales.Domain.Tests/SnackRepositoryGainTests.cs
+++ Tests/EMachine.Sales.Domain.Tests/SnackRepositoryGainTests.cs
@@ -23,27 +23,49 @@ public class SnackRepositoryGainTests
     [Fact]
     public async Task Can_Create_Snack()
     {
-        var id = Guid.NewGuid();
+        var id = new Guid("0edbbe28-116d-4c0e-9f64-baebb91f7500");
         var grain = _cluster.GrainFactory.GetGrain<ISnackRepositoryGrain>(Guid.Empty);
-        var createResult = await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(id, "Cafe", Guid.NewGuid(), "Leo"));
+        var createResult = await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(id, "Apple", Guid.NewGuid(), "Leo"));
         createResult.IsSuccess.Should().Be(true);
         createResult.Value.Id.Should().Be(id);
-        createResult.Value.Name.Should().Be("Cafe");
+        createResult.Value.Name.Should().Be("Apple");
         createResult.Value.CreatedBy.Should().Be("Leo");
         _testOutputHelper.WriteLine(createResult.ToString());
     }
     
     [Fact]
+    public async Task Can_Delete_Snack()
+    {
+        var id = new Guid("13ba54cc-ae13-4de8-a460-d9fcd17a3674");
+        var grain = _cluster.GrainFactory.GetGrain<ISnackRepositoryGrain>(Guid.Empty);
+        var createResult = await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(id, "Lemon", Guid.NewGuid(), "Leo"));
+        createResult.IsSuccess.Should().Be(true);
+        var deleteResult = await grain.DeleteSnackAsync(new SnackRepositoryDeleteOneCommand(id, Guid.NewGuid(), "Boss"));
+        deleteResult.IsSuccess.Should().Be(true);
+        _testOutputHelper.WriteLine(deleteResult.ToString());
+    }
+
+    [Fact]
+    public async Task Can_Get_Snack()
+    {
+        var grain = _cluster.GrainFactory.GetGrain<ISnackRepositoryGrain>(Guid.Empty);
+        var getResult = await grain.GetSnackAsync(new SnackRepositoryGetOneCommand(new Guid("a74ad750-0d8b-4ede-ad29-e5e0d5c2dd9e"), Guid.NewGuid(), "Boss"));
+        getResult.IsSuccess.Should().BeTrue();
+        getResult.Value.Id.Should().Be(new Guid("a74ad750-0d8b-4ede-ad29-e5e0d5c2dd9e"));
+        getResult.Value.Name.Should().Be("Cafe");
+    }
+
+    [Fact]
     public async Task Can_Get_Snacks()
     {
         var grain = _cluster.GrainFactory.GetGrain<ISnackRepositoryGrain>(Guid.Empty);
-        await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("a74ad750-0d8b-4ede-ad29-e5e0d5c2dd9e"), "Cafe", Guid.NewGuid(), "Leo"));
-        await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("7db1925a-4458-479f-9c6b-1f708c35b29e"), "Chocolate", Guid.NewGuid(), "Leo"));
-        await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("d5753343-78d4-4c4a-a674-63b75f096059"), "Soda", Guid.NewGuid(), "Leo"));
-        await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("83d423b6-96a3-48fd-b9e7-97a00bb98e2f"), "Gum", Guid.NewGuid(), "Leo"));
+        // await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("a74ad750-0d8b-4ede-ad29-e5e0d5c2dd9e"), "Cafe", Guid.NewGuid(), "Leo"));
+        // await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("7db1925a-4458-479f-9c6b-1f708c35b29e"), "Chocolate", Guid.NewGuid(), "Leo"));
+        // await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("d5753343-78d4-4c4a-a674-63b75f096059"), "Soda", Guid.NewGuid(), "Leo"));
+        // await grain.CreateSnackAsync(new SnackRepositoryCreateOneCommand(new Guid("83d423b6-96a3-48fd-b9e7-97a00bb98e2f"), "Gum", Guid.NewGuid(), "Leo"));
         var getResult = await grain.GetSnacksAsync(new SnackRepositoryGetListCommand(100, 0, Guid.NewGuid(), "Boss"));
         getResult.IsSuccess.Should().BeTrue();
-        getResult.Value.Should().HaveCount(4);
+        getResult.Value.Count.Should().BeGreaterOrEqualTo(4);
         getResult.Value.ForEach(x => _testOutputHelper.WriteLine(x.ToString()));
     }
 }
