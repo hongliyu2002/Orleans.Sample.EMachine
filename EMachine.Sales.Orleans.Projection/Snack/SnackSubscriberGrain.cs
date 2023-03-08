1bc7da2227c764c68005169e50336ea4e1dbbfe4
Leo Hong <hongliyu2002@hotmail.com>
2023-03-09 05:31:44 +0800

    Update

--- EMachine.Sales.Orleans.Projection/Snack/SnackSubscriberGrain.cs
+++ EMachine.Sales.Orleans.Projection/Snack/SnackSubscriberGrain.cs
@@ -7,6 +7,7 @@ using Fluxera.Guards;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Orleans.Streams;
+using Polly;
 
 namespace EMachine.Sales.Orleans.Projection;
 
@@ -34,11 +35,11 @@ public sealed class SnackSubscriberGrain : EventSubscriberGrain
         switch (evt)
         {
             case SnackInitializedEvent snackEvt:
-                return await ApplyEventAsync(snackEvt);
+                return await ApplyEventWithRetryAndFallbackAsync(snackEvt, (e, ct) => ApplyEventAsync((SnackInitializedEvent)e, ct));
             case SnackRemovedEvent snackEvt:
-                return await ApplyEventAsync(snackEvt);
+                return await ApplyEventWithRetryAndFallbackAsync(snackEvt, (e, ct) => ApplyEventAsync((SnackRemovedEvent)e, ct));
             case SnackNameChangedEvent snackEvt:
-                return await ApplyEventAsync(snackEvt);
+                return await ApplyEventWithRetryAndFallbackAsync(snackEvt, (e, ct) => ApplyEventAsync((SnackNameChangedEvent)e, ct));
             case ErrorOccurredEvent errorEvt:
                 _logger.LogWarning(errorEvt.Message);
                 return true;
@@ -62,83 +63,95 @@ public sealed class SnackSubscriberGrain : EventSubscriberGrain
 
     #region Apply
 
-    private async Task<bool> ApplyEventAsync(SnackInitializedEvent evt)
+    private async Task<bool> ApplyEventWithRetryAndFallbackAsync(SnackEvent evt, Func<SnackEvent, CancellationToken, Task<bool>> applyEvent, CancellationToken cancellationToken = default)
     {
-        var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
-        if (snackInDb != null)
-        {
-            _logger.LogWarning($"Snack {evt.Id} already exists.");
-            return false;
-        }
-        var snack = new Snack
-                    {
-                        Id = evt.Id,
-                        Name = evt.Name,
-                        CreatedAt = evt.OperatedAt,
-                        CreatedBy = evt.OperatedBy
-                    };
-        await _dbContext.Snacks.AddAsync(snack);
-        var savedNumber = await _dbContext.SaveChangesAsync();
-        return savedNumber > 0;
+        var retryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(3, retry => TimeSpan.FromSeconds(Math.Pow(2, retry)));
+        var fallbackPolicy = Policy.Handle<Exception>().FallbackAsync(async cancelToken => await ApplyFallbackAsync(evt, cancelToken));
+        var policy = Policy.WrapAsync(retryPolicy, fallbackPolicy);
+        return await policy.ExecuteAsync(async cancelToken => await applyEvent(evt, cancelToken), cancellationToken);
     }
 
-    private async Task<bool> ApplyEventAsync(SnackRemovedEvent evt)
+    private async Task<bool> ApplyEventAsync(SnackInitializedEvent evt, CancellationToken cancellationToken = default)
     {
         var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
         if (snackInDb == null)
         {
-            _logger.LogWarning($"Snack {evt.Id} does not exist.");
+            var snack = new Snack
+                        {
+                            Id = evt.Id,
+                            Name = evt.Name,
+                            CreatedAt = evt.OperatedAt,
+                            CreatedBy = evt.OperatedBy
+                        };
+            await _dbContext.Snacks.AddAsync(snack, cancellationToken);
+        }
+        var savedNumber = await _dbContext.SaveChangesAsync(cancellationToken);
+        return savedNumber > 0;
+    }
+
+    private async Task<bool> ApplyEventAsync(SnackRemovedEvent evt, CancellationToken cancellationToken = default)
+    {
+        var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
+        if (snackInDb == null)
+        {
+            _logger.LogWarning($"Apply event SnackRemovedEvent: Snack {evt.Id} does not exist.");
             return false;
         }
         snackInDb.DeletedAt = evt.OperatedAt;
         snackInDb.DeletedBy = evt.OperatedBy;
         snackInDb.IsDeleted = true;
-        var savedNumber = await _dbContext.SaveChangesAsync();
+        var savedNumber = await _dbContext.SaveChangesAsync(cancellationToken);
         return savedNumber > 0;
     }
 
-    private async Task<bool> ApplyEventAsync(SnackNameChangedEvent evt)
+    private async Task<bool> ApplyEventAsync(SnackNameChangedEvent evt, CancellationToken cancellationToken = default)
     {
         var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
         if (snackInDb == null)
         {
-            _logger.LogWarning($"Snack {evt.Id} does not exist.");
+            _logger.LogWarning($"Apply event SnackNameChangedEvent: Snack {evt.Id} does not exist.");
             return false;
         }
         snackInDb.Name = evt.Name;
         snackInDb.LastModifiedAt = evt.OperatedAt;
         snackInDb.LastModifiedBy = evt.OperatedBy;
-        var savedNumber = await _dbContext.SaveChangesAsync();
+        var savedNumber = await _dbContext.SaveChangesAsync(cancellationToken);
         return savedNumber > 0;
     }
 
-    private async Task<bool> ApplyFallbackAsync(SnackEvent evt)
+    private async Task ApplyFallbackAsync(SnackEvent evt, CancellationToken cancellationToken = default)
     {
-        var snackGrain = GrainFactory.GetGrain<ISnackGrain>(evt.Id);
-        var snackResult = await snackGrain.GetAsync();
-        if (snackResult.IsFailed)
+        try
         {
-            _logger.LogWarning($"Snack grain.{evt.Id} does not exist. Reasons: {snackResult}");
-            return false;
+            var snackGrain = GrainFactory.GetGrain<ISnackGrain>(evt.Id);
+            var snackResult = await snackGrain.GetAsync();
+            if (snackResult.IsFailed)
+            {
+                _logger.LogWarning($"Apply Fallback: Snack grain.{evt.Id} does not exist. Reasons: {snackResult}");
+                return;
+            }
+            var snackInGrain = snackResult.Value;
+            var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
+            if (snackInDb == null)
+            {
+                snackInDb = new Snack();
+                await _dbContext.Snacks.AddAsync(snackInDb, cancellationToken);
+            }
+            snackInDb.Id = snackInGrain.Id;
+            snackInDb.Name = snackInGrain.Name;
+            snackInDb.CreatedAt = snackInGrain.CreatedAt;
+            snackInDb.LastModifiedAt = snackInGrain.LastModifiedAt;
+            snackInDb.DeletedAt = snackInGrain.DeletedAt;
+            snackInDb.CreatedBy = snackInGrain.CreatedBy;
+            snackInDb.LastModifiedBy = snackInGrain.LastModifiedBy;
+            snackInDb.DeletedBy = snackInGrain.DeletedBy;
+            snackInDb.IsDeleted = snackInGrain.IsDeleted;
+            await _dbContext.SaveChangesAsync(cancellationToken);
         }
-        var snackInGrain = snackResult.Value;
-        var snackInDb = await _dbContext.Snacks.FindAsync(evt.Id);
-        if (snackInDb == null)
+        catch (Exception ex)
         {
-            snackInDb = new Snack();
-            await _dbContext.Snacks.AddAsync(snackInDb);
+            _logger.LogWarning(ex, $"Apply Fallback: {ex.Message}");
         }
-        snackInDb.Id = snackInGrain.Id;
-        snackInDb.Name = snackInGrain.Name;
-        snackInDb.CreatedAt = snackInGrain.CreatedAt;
-        snackInDb.LastModifiedAt = snackInGrain.LastModifiedAt;
-        snackInDb.DeletedAt = snackInGrain.DeletedAt;
-        snackInDb.CreatedBy = snackInGrain.CreatedBy;
-        snackInDb.LastModifiedBy = snackInGrain.LastModifiedBy;
-        snackInDb.DeletedBy = snackInGrain.DeletedBy;
-        snackInDb.IsDeleted = snackInGrain.IsDeleted;
-        var savedNumber = await _dbContext.SaveChangesAsync();
-        return savedNumber > 0;
     }
 
     #endregion
